c2f0ec15b0372c14eb8ffe16a88b22f52abe150e
Allan <allanfelipedk@gmail.com>
2022-08-30 19:44:48 -0300

    Creating an account on the photon engine website and downloading the PUN 2 assets

--- Assets/Runtime/Script/PlayerController.cs
+++ Assets/Runtime/Script/PlayerController.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 0.2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float jumpSpeed = 0.5f;
     void Update()
     {
         PlayerMovement();
     }
-
     private void PlayerMovement()
     {
         if (Input.GetKey(KeyCode.D))
         {
+            GetComponent<SpriteRenderer>().flipX = false;
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
+            GetComponent<SpriteRenderer>().flipX = true;
             transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * jumpSpeed * Time.deltaTime;
         }
     }
 }
